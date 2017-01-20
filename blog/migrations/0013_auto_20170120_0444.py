313244165fd7709deb1d5ac8026bd70bb91957f0
tuerlefl <florian.tuerler@hotmail.com>
2017-01-20 11:46:05 +0100

    makemigrations allink-apps/ brands

---
+++ blog/migrations/0013_auto_20170120_0444.py
@@ -0,0 +1,32 @@
+# -*- coding: utf-8 -*-
+from __future__ import unicode_literals
+
+from django.db import migrations, models
+import filer.fields.image
+import allink_core.allink_base.models.model_fields
+
+
+class Migration(migrations.Migration):
+
+    dependencies = [
+        ('filer', '0006_auto_20160623_1627'),
+        ('blog', '0012_auto_20170112_0632'),
+    ]
+
+    operations = [
+        migrations.AddField(
+            model_name='blogappcontentplugin',
+            name='bg_image_outer_container',
+            field=filer.fields.image.FilerImageField(related_name='blog_blogappcontentplugin_bg_image', blank=True, to='filer.Image', help_text='Dimensions TBD', null=True, verbose_name='Background-Image'),
+        ),
+        migrations.AlterField(
+            model_name='blogappcontentplugin',
+            name='bg_color',
+            field=models.IntegerField(null=True, verbose_name='Set a predefined background color', blank=True),
+        ),
+        migrations.AlterField(
+            model_name='blogappcontentplugin',
+            name='extra_css_classes',
+            field=allink_core.allink_base.models.model_fields.Classes(default=b'', help_text='Space separated classes that are added to the class. See <a href="http://getbootstrap.com/css/" target="_blank">Bootstrap 3 documentation</a>.', verbose_name='Css Classes', blank=True),
+        ),
+    ]
