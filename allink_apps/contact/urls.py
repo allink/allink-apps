fbc210db8844737b7c74b73dc46a665a0e653854
ekesto <beat@ekesto.com>
2017-04-03 10:20:24 +0200

    removing allink_apps folder and content

--- allink_apps/contact/urls.py
+++
@@ -1,8 +0,0 @@
-# # -*- coding: utf-8 -*-
-from django.conf.urls import url
-from .views import ContactRequestView
-
-
-urlpatterns = [
-    url(r'^request/$', ContactRequestView.as_view(), name='request'),
-]
--- allink_apps/people/sitemaps/__init__.py
+++
@@ -1,1 +0,0 @@
-from sitemaps import PeopleSitemap
