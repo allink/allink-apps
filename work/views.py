5996b22faeed01a841e3c9f5a36430bbd01b4329
tuerlefl <florian.tuerler@hotmail.com>
2017-05-22 12:14:23 +0200

    work pdf done

--- work/views.py
+++ work/views.py
@@ -1,8 +1,10 @@
 # -*- coding: utf-8 -*-
+import datetime
+from django.http import HttpResponse
 
 from allink_core.allink_base.views import AllinkBasePluginLoadMoreView, AllinkBaseDetailView
 from allink_apps.work.models import Work, WorkAppContentPlugin
-
+from .pdf import PdfWork
 
 class WorkPluginLoadMore(AllinkBasePluginLoadMoreView):
     model = Work
@@ -11,3 +13,17 @@ class WorkPluginLoadMore(AllinkBasePluginLoadMoreView):
 
 class WorkDetail(AllinkBaseDetailView):
     model = Work
+
+
+def export_pdf(request, id):
+    date = (datetime.date.today().strftime('%d-%m-%Y'))
+
+    item = Work.objects.get(id=id)
+
+    pdf = PdfWork(item, request)
+    output = pdf.build()
+    filename = '%s_%s.pdf' % (item.title, date)
+
+    response = HttpResponse(output.getvalue(), content_type="application/pdf")
+    response['Content-Disposition'] = "attachment; filename=%s" % filename
+    return response
